6b9dd65eb1de4b3777c9760ae59c12a5af8db6e1
jackcdavey <johncdavey2000@gmail.com>
2026-02-16 17:11:16 -0800

    Config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = "Flush-Force-One-Website";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
